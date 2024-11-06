1192bef6aaa88838ea1471da8db381f234909d2d
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-07 00:09:05 +0300

    website went public

--- TripLogClient/src/app/constants.ts
+++ TripLogClient/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const api:string="http://localhost:5253/api";
-
+// // export const api:string="http://172.31.32.97:2020/api";
+export const api:string="http://16.171.148.214/api";
 export const tripsImage:string="http://localhost:5253/trips/";
 export const contentsImage:string="http://localhost:5253/contents/";
--- TripLogServer/TripLogServer.WebAPI/Program.cs
+++ TripLogServer/TripLogServer.WebAPI/Program.cs
@@ -17,11 +17,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
