2a67ed8fb029671d6e0176eedc22fb017a86f15d
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-07 13:55:41 +0300

    constants updated

--- TripLogClient/src/app/constants.ts
+++ TripLogClient/src/app/constants.ts
@@ -1,4 +1,4 @@
 //export const api:string="http://localhost:5253/api";
-export const api:string="http://13.48.194.206/api";
+export const api:string="https://xqjqd33e8j.execute-api.eu-north-1.amazonaws.com/api";
 export const tripsImage:string="http://localhost:5253/trips/";
 export const contentsImage:string="http://localhost:5253/contents/";
